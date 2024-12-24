14bcf01a8393b5b64c6de2013a61fbabc0584804
a'Laa <125521519+aLaa-KabiL@users.noreply.github.com>
2024-12-24 21:25:06 +0200

    Final Edit

---
+++ test-hotel-main/test-hotel-main/Rahhal.js
@@ -0,0 +1,18 @@
+// let content =document.querySelector(".content");
+
+
+// window.onscroll = function() {
+//     let value=scrollY;
+
+// content.style.fontSize=value + "px";
+//     if(scrollY>=96){
+//         content.style.fontSize=96 + "px";
+//         content.style.position='fixed';
+//         if(scrollY>=480){
+//             content.style.display='none';
+
+//         }else{
+//             content.style.display='block';
+//         }
+//     }
+// }
